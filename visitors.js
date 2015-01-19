371ad39d63a75d9b92b03f1cbf77b12cd64efb75
gcanti <giulio.canti@gmail.com>
2015-01-19 10:57:17 +0100

    added function support

--- visitors.js
+++ visitors.js
@@ -10,102 +10,139 @@ function debug(x) {
   console.log(JSON.stringify(x, null, 2));
 }
 
-function commentOut(range, state) {
-  utils.catchup(range[0], state);
-  utils.append(' /* ', state);
-  utils.catchup(range[1], state);
-  utils.append(' */', state);
-}
-
 function getOption(name, state) {
   return state.g.opts[name];
 }
 
-function getProperty(name, typeAssertionVariable) {
+function getProperty(name, tav) {
   return name in {'void': 1, 'boolean': 1} ? // compatibility with ES3
-    typeAssertionVariable + '["' + name + '"]' :
-    typeAssertionVariable + '.' + name;
+    tav + '["' + name + '"]' :
+    tav + '.' + name;
 }
 
-function getType(ann, t) {
-  switch (ann.type) {
-    case Syntax.StringTypeAnnotation :
-      return getProperty('string', t);
-    case Syntax.NumberTypeAnnotation :
-      return getProperty('number', t);
-    case Syntax.BooleanTypeAnnotation :
-      return getProperty('boolean', t);
-    case Syntax.AnyTypeAnnotation :
-      return getProperty('any', t);
-    case Syntax.VoidTypeAnnotation :
-      return getProperty('void', t);
-    case Syntax.NullableTypeAnnotation :
-      // handle ?T
-      return getProperty('maybe', t) + '(' + getType(ann.typeAnnotation, t) + ')';
-    case Syntax.ArrayTypeAnnotation :
-      // handle T[]
-      return getProperty('list', t) + '(' + getType(ann.elementType, t) + ')';
-    case Syntax.GenericTypeAnnotation :
-      if (ann.id.type === Syntax.Identifier) {
-        var id = ann.id.name;
-        // handle mixed type
-        if (id === 'mixed') {
-          return getProperty('mixed', t);
+function getType(ann, tav) {
+  if (ann) {
+    switch (ann.type) {
+
+      case Syntax.StringTypeAnnotation :
+        return getProperty('string', tav);
+      case Syntax.NumberTypeAnnotation :
+        return getProperty('number', tav);
+      case Syntax.BooleanTypeAnnotation :
+        return getProperty('boolean', tav);
+      case Syntax.AnyTypeAnnotation :
+        return getProperty('any', tav);
+      case Syntax.VoidTypeAnnotation :
+        return getProperty('void', tav);
+
+      case Syntax.NullableTypeAnnotation :
+        // handle ?T
+        return getProperty('maybe', tav) + '(' + getType(ann.typeAnnotation, tav) + ')';
+
+      case Syntax.ArrayTypeAnnotation :
+        // handle T[]
+        return getProperty('list', tav) + '(' + getType(ann.elementType, tav) + ')';
+
+      case Syntax.GenericTypeAnnotation :
+        if (ann.id.type === Syntax.Identifier) {
+          var id = ann.id.name;
+          // handle mixed type
+          if (id === 'mixed') {
+            return getProperty('mixed', tav);
+          }
+          if (ann.typeParameters) {
+            // handle Array<T>
+            var params = ann.typeParameters.params;
+            if (params.length === 1) {
+              return getProperty('list', tav) + '(' + getType(params[0], tav) + ')';
+            }
+          }
+          // handle generic T, es: `var a: Person`
+          return id;
         }
-        // handle Array<T>
-        var params = ann.typeParameters.params;
-        if (params.length === 1) {
-          // add list and recurse
-          return getProperty('list', t) + '(' + getType(params[0], t) + ')';
+
+      case Syntax.TupleTypeAnnotation :
+        // handle [T1, T2, ... , Tn]
+        return getProperty('tuple', tav) + '([' + ann.types.map(function (type) {
+          return getType(type, tav);
+        }).join(', ') + '])';
+
+      case Syntax.ObjectTypeAnnotation :
+        if (ann.properties.length) {
+          // handle {p1: T1; p2: T2; ... pn: Tn;}
+          return getProperty('object', tav) + '({' + ann.properties.map(function (prop) {
+            return prop.key.name + ': ' + getType(prop.value, tav);
+          }).join(', ') + '})';
+        } else if (ann.indexers.length === 1) {
+          // handle {[key: D]: C}
+          var domain = getType(ann.indexers[0].key, tav);
+          var codomain = getType(ann.indexers[0].value, tav);
+          return getProperty('dict', tav) + '(' + domain + ', ' + codomain + ')';
         }
-      }
-    case Syntax.TupleTypeAnnotation :
-      // handle [T1, T2, ... , Tn]
-      return getProperty('tuple', t) + '([' + ann.types.map(function (type) {
-        return getType(type, t);
-      }).join(', ') + '])';
-    case Syntax.ObjectTypeAnnotation :
-      if (ann.properties.length) {
-        // handle {p1: T1; p2: T2; ... pn: Tn;}
-        return getProperty('object', t) + '({' + ann.properties.map(function (prop) {
-          return prop.key.name + ': ' + getType(prop.value, t);
-        }).join(', ') + '})';
-      } else if (ann.indexers.length === 1) {
-        // handle {[key: D]: C}
-        var domain = getType(ann.indexers[0].key, t);
-        var codomain = getType(ann.indexers[0].value, t);
-        return getProperty('dict', t) + '(' + domain + ', ' + codomain + ')';
-      }
-    case Syntax.UnionTypeAnnotation :
-      // handle T1 | T2 | ... | Tn
-      return getProperty('union', t) + '([' + ann.types.map(function (type) {
-        return getType(type, t);
-      }).join(', ') + '])';
+
+      case Syntax.UnionTypeAnnotation :
+        // handle T1 | T2 | ... | Tn
+        return getProperty('union', tav) + '([' + ann.types.map(function (type) {
+          return getType(type, tav);
+        }).join(', ') + '])';
+    }
   }
-  return 't.any /* [WARNING] ' + ann.type + ' */';
+  return getProperty('any', tav);
 }
 
-function visitVariableDeclarator(traverse, node, path, state) {
-  var t = getOption('typeAssertionVariable', state);
+function visitTypedVariableDeclarator(traverse, node, path, state) {
+
+  var tav = getOption('typeAssertionVariable', state);
   var ann = node.id.typeAnnotation;
-  if (getOption('commentTypes', state)) {
-    commentOut(ann.range, state);
-  }
-  if (node.init && getOption('typeAssertions', state)) {
+
+  if (node.init) {
     utils.catchup(node.init.range[0], state);
-    utils.append(getProperty('check', t) + '(', state);
+    utils.append(getProperty('check', tav) + '(', state);
+    traverse(node.init, path, state);
     utils.catchup(node.init.range[1], state);
-    utils.append(', ' + getType(ann.typeAnnotation, t) + ')', state);
+    utils.append(', ' + getType(ann.typeAnnotation, tav) + ')', state);
   }
+
+  utils.catchup(node.range[1], state);
+  return false;
 }
-visitVariableDeclarator.test = function(node, path, state) {
+visitTypedVariableDeclarator.test = function(node, path, state) {
   return node.type === Syntax.VariableDeclarator &&
     node.id.typeAnnotation;
 };
 
+function visitTypedFunction(traverse, node, path, state) {
+
+  var tav = getOption('typeAssertionVariable', state);
+  var types = [];
+  var params = [];
+  var rest = node.rest ? ', ' + getType(node.rest.typeAnnotation.typeAnnotation, tav) : '';
+  node.params.forEach(function (param) {
+    types.push(getType(param.typeAnnotation ? param.typeAnnotation.typeAnnotation : null, tav));
+    params.push(param.name);
+  });
+
+  utils.catchup(node.body.range[0] + 1, state);
+  utils.append(tav + '.check(arguments, ' + tav + '.args([' + types.join(', ') + ']' + rest + '));', state);
+
+  if (node.returnType) {
+    var returnType = getType(node.returnType.typeAnnotation, tav);
+    utils.append(' var ret = (function (' + params.join(', ') + ') {', state);
+    utils.catchup(node.body.range[1], state);
+    utils.append(').apply(this, arguments); return ' + tav + '.check(ret, ' + returnType + ');}', state);
+  }
+
+  utils.catchup(node.range[1], state);
+  return false;
+}
+visitTypedFunction.test = function(node, path, state) {
+  return (node.type === Syntax.FunctionDeclaration || node.type === Syntax.FunctionExpression) &&
+  (node.returnType || node.params.some(function (param) { return !!param.typeAnnotation; }));
+};
+
 module.exports = {
-  getType: getType,
   visitorList: [
-    visitVariableDeclarator
+    visitTypedFunction,
+    visitTypedVariableDeclarator
   ]
 };
