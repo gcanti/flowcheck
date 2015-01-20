721d7149ab8f40c9d1d2dece062f2051b82da821
gcanti <giulio.canti@gmail.com>
2015-01-20 15:19:43 +0100

    added docs

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
-flowcheck modules:
+# Why?
+
+Use types annotations today, even if you don't actually use Flow (or TypeScript). Then strip the assertions in production.
+
+Flowcheck comes with a built-in browserify transformer for a seamless workflow integration.
+
+You can even use Flowcheck as a general purpose validation library.
+
+# How it works?
+
+Flowcheck adds asserts for each type annotation using jstransform.
+
+If an assert fails the debugger kicks in so you can inspect the stack and quickly find out what's wrong.
+
+# Workflow
+
+1. Write your code adding type annotations
+2. (optional) enable the Flow static type checker
+3. enable Flowcheck.js and strip type annotations (e.g. with jsx or 6to5) during development
+4. disable Flowcheck.js in production for zero overhead
+
+# Modules
 
 - a **source transformer** (transform.js)
 - a **browserify transformer** (index.js)
 - a **runtime assertion library** (assert.js)
 
-The transformer adds an assertion for each type annotation.
+The transformer adds assertions for each type annotation.
 
 The assertion module checks the types at runtime. If an assert fails **the debugger kicks in** so you can inspect the stack and quickly find out what's wrong.
 
@@ -29,13 +50,9 @@ Failure = {
 }
 ```
 
-So you can also use flowcheck's assert module as a **general purpose validation** library.
-
 # API
 
 `transform(source: string, options: ?object): string`
-
-Inserts the type assertions.
 
 Options
 
@@ -44,9 +61,9 @@ Options
 
 # Assert library API
 
-`f.irreducible(name: string, is: (x: any) => boolean): Type`
+`f.define(name: string, is: (x: any) => boolean): Type`
 
-Defines a new irreducible type,
+Defines a new basic type.
 
 `f.check(value: T, type: Type): T`
 
--- docs/demo/index.jsx
+++ docs/demo/index.jsx
@@ -3,7 +3,7 @@
 var React = require('react');
 var CodeMirror = require('react-code-mirror');
 require('codemirror/mode/javascript/javascript');
-var transform = require('../../transform');
+var transform = require('../../transform').transform;
 var beautify = require('js-beautify');
 window.f = require('../../assert');
 
@@ -25,16 +25,22 @@ var App = React.createClass({
   },
 
   compile: function (value, options) {
+    options = options || this.state;
     var code = value;
     try {
       // flowcheck
-      if (this.state.assertions) {
-        code = transform(code);
+      if (options.assertions) {
+        code = transform(code, {
+          assertions: options.assertions
+        });
       }
       // jsx
-      code = JSXTransformer.transform(code, options || this.state).code;
+      code = JSXTransformer.transform(code, {
+        harmony: options.harmony,
+        stripTypes: options.stripTypes
+      }).code;
       // beautify
-      if (this.state.beautify) {
+      if (options.beautify) {
         code = beautify(code, {
           indent_size: 2
         });
@@ -46,7 +52,10 @@ var App = React.createClass({
   },
 
   onSourceChange: function (evt) {
-    this.setState({value: evt.target.value});
+    this.setState({
+      value: evt.target.value,
+      error: null
+    });
   },
 
   onAssertionsChange: function (evt) {
@@ -85,65 +94,50 @@ var App = React.createClass({
       </div>
     ) : null;
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-5">
-            <h1><a href="https://github.com/gcanti/flowcheck">flowcheck</a> compiler</h1>
-            <p className="lead">Gradual type checking for Flow and TypeScript </p>
-          </div>
-          <div className="col-md-7">
-            <h3>Why?</h3>
-            <p><b>Use types today</b>, even if you don't use Flow or TypeScript, then strip the assertions in production.</p>
-            <h3>How it works?</h3>
-            <p>It adds an <b>assertion for each type annotation</b>.
-            The assertion module <code>f</code> checks the types at runtime. If an assert fails <b>the debugger kicks in</b> so you can inspect the stack and quickly find out what's wrong.</p>
+      <div className="row">
+        <div className="col-md-6">
+          <p><b>Source</b> (live code editor)</p>
+          <div className="form-group">
+            <CodeMirror
+              tabSize={2}
+              style={{border: '1px solid #F6E4CC'}}
+              textAreaClassName={['form-control']}
+              mode="javascript"
+              value={this.state.value}
+              onChange={this.onSourceChange} />
           </div>
         </div>
-        <div className="row">
-          <div className="col-md-5">
-            <p><b>Source</b></p>
-            <div className="form-group">
-              <CodeMirror
-                tabSize={2}
-                style={{border: '1px solid #F6E4CC'}}
-                textAreaClassName={['form-control']}
-                mode="javascript"
-                value={this.state.value}
-                onChange={this.onSourceChange} />
-            </div>
+        <div className="col-md-6">
+          <p><b>Output</b></p>
+          <div className="form-group">
+            <CodeMirror
+              tabSize={2}
+              readOnly={true}
+              style={{border: '1px solid #F6E4CC'}}
+              textAreaClassName={['form-control']}
+              mode="javascript"
+              value={code}
+              smartIndent={false} />
           </div>
-          <div className="col-md-7">
-            <p><b>Output</b></p>
-            <div className="form-group">
-              <CodeMirror
-                tabSize={2}
-                readOnly={true}
-                style={{border: '1px solid #F6E4CC'}}
-                textAreaClassName={['form-control']}
-                mode="javascript"
-                value={code}
-                smartIndent={false} />
-            </div>
-            <div className="form-group">
-              <label className="checkbox-inline">
-                <input type="checkbox" id="assertions" checked={this.state.assertions} onChange={this.onAssertionsChange} /> assertions
-              </label>
-              <label className="checkbox-inline">
-                <input type="checkbox" id="harmony" checked={this.state.harmony} onChange={this.onHarmonyChange} /> harmony
-              </label>
-              <label className="checkbox-inline">
-                <input type="checkbox" id="stripTypes" checked={this.state.stripTypes} onChange={this.onStripTypesChange} /> stripTypes
-              </label>
-              <label className="checkbox-inline">
-                <input type="checkbox" id="beautify" checked={this.state.beautify} onChange={this.onBeautifyChange} /> beautify
-              </label>
-            </div>
-            <div className="form-group">
-              <button className="btn btn-primary" onClick={this.run}>Run (output to the console)</button>
-            </div>
-            <div className="form-group">
-              {alert}
-            </div>
+          <div className="form-group">
+            <label className="checkbox-inline">
+              <input type="checkbox" id="assertions" checked={this.state.assertions} onChange={this.onAssertionsChange} /> assertions
+            </label>
+            <label className="checkbox-inline">
+              <input type="checkbox" id="harmony" checked={this.state.harmony} onChange={this.onHarmonyChange} /> harmony
+            </label>
+            <label className="checkbox-inline">
+              <input type="checkbox" id="stripTypes" checked={this.state.stripTypes} onChange={this.onStripTypesChange} /> stripTypes
+            </label>
+            <label className="checkbox-inline">
+              <input type="checkbox" id="beautify" checked={this.state.beautify} onChange={this.onBeautifyChange} /> beautify
+            </label>
+          </div>
+          <div className="form-group">
+            <button className="btn btn-danger" onClick={this.run}>Run (output to the console)</button>
+          </div>
+          <div className="form-group">
+            {alert}
           </div>
         </div>
       </div>
